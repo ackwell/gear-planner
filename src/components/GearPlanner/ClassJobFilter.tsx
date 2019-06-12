c6c0ec1856cec863ad6994755a9c63ab1756b58e
Saxon Landers <saxon@ackwell.com.au>
2019-06-12 23:51:34 +1000

    Move CJ filter into own file

---
+++ src/components/GearPlanner/ClassJobFilter.tsx
@@ -0,0 +1,43 @@
+import {LoadingState, GlobalStoreContext} from '../../state'
+import React from 'react'
+import {observer} from 'mobx-react-lite'
+import {ClassJob} from '../../api'
+
+interface Props {
+	onSelect?: (classJob: ClassJob) => void
+}
+
+export const ClassJobFilter = observer((props: Props) => {
+	const {classJobStore} = React.useContext(GlobalStoreContext)
+	classJobStore.ensure()
+
+	const onSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const {onSelect} = props
+		if (!onSelect) {
+			return
+		}
+		const id = Number(event.target.value)
+		const cj = classJobStore.classJobs.find(cj => cj.id === id)
+		cj && onSelect(cj)
+	}
+
+	// TODO: break this up a bit?
+	return (
+		<>
+			{LoadingState[classJobStore.state]}
+			<select onChange={onSelect}>
+				{classJobStore.categories.map(cat => (
+					<optgroup key={cat.id} label={cat.name}>
+						{classJobStore.classJobs
+							.filter(cj => cj.category.id === cat.id)
+							.map(cj => (
+								<option key={cj.id} value={cj.id}>
+									{cj.name}
+								</option>
+							))}
+					</optgroup>
+				))}
+			</select>
+		</>
+	)
+})
--- src/components/GearPlanner/GearPlanner.tsx
+++ src/components/GearPlanner/GearPlanner.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import {GlobalStoreContext, LoadingState} from '../../state'
+import {GlobalStoreContext} from '../../state'
 import {observer} from 'mobx-react-lite'
 import {ClassJob} from '../../api'
+import {ClassJobFilter} from './ClassJobFilter'
 
 export const GearPlanner = observer(() => {
 	const {gearPlannerStore} = React.useContext(GlobalStoreContext)
@@ -16,45 +17,6 @@ export const GearPlanner = observer(() => {
 				gear planner ({gearPlannerStore.classJob && gearPlannerStore.classJob.abbr})
 			</h2>
 			<ClassJobFilter onSelect={onSelect} />
-		</>
-	)
-})
-
-interface ClassJobFilterProps {
-	onSelect?: (classJob: ClassJob) => void
-}
-
-const ClassJobFilter = observer((props: ClassJobFilterProps) => {
-	const {classJobStore} = React.useContext(GlobalStoreContext)
-	classJobStore.ensure()
-
-	const onSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		const {onSelect} = props
-		if (!onSelect) {
-			return
-		}
-		const id = Number(event.target.value)
-		const cj = classJobStore.classJobs.find(cj => cj.id === id)
-		cj && onSelect(cj)
-	}
-
-	// TODO: break this up a bit?
-	return (
-		<>
-			{LoadingState[classJobStore.state]}
-			<select onChange={onSelect}>
-				{classJobStore.categories.map(cat => (
-					<optgroup key={cat.id} label={cat.name}>
-						{classJobStore.classJobs
-							.filter(cj => cj.category.id === cat.id)
-							.map(cj => (
-								<option key={cj.id} value={cj.id}>
-									{cj.name}
-								</option>
-							))}
-					</optgroup>
-				))}
-			</select>
 		</>
 	)
 })
